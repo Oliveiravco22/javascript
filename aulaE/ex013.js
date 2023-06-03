12e1835faced187446127fb09868d251c3a763b2
André Oliveira <130932501+Oliveiravco22@users.noreply.github.com>
2023-06-03 17:34:58 -0300

    novo exercicio

---
+++ aulaE/ex013.js
@@ -0,0 +1,11 @@
+// alert('Testando')
+function conta() {
+    var ini = window.document.getElementById('inicio')
+    var fim = window.document.getElementById('fim')
+    var pas = window.document.getElementById('pass')
+    var res = window.document.getElementById('res')
+
+   
+        res.innerHTML = 'ini+pas'
+    
+}    
